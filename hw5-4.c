f06608c1c4fcc31b867776690a2c028f41fff5a7
zheng0225 <146065408+zheng0225@users.noreply.github.com>
2023-11-06 22:21:52 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 
+int main() {
+    int H, M;
+    float angle;
+    scanf("%d %d", &H, &M);
+
+    if (H < 1 || H > 12 || M < 0 || M > 59) {
+        printf("輸入的時間不符合條件。\n");
+        return 1;
+    }
+
+    float h = (H % 12) * 30 + M * 0.5;
+    float m = M * 6;
+
+
+    angle = fabs(h- m);
+
+    if (angle > 180) {
+        angle = 360 - angle;
+    }
+
+    printf("%.3f\n", angle);
+
+    return 0;
+}
