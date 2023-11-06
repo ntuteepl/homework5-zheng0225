96ee2bda766b9fbd75b4c0f554121151a0905939
zheng0225 <146065408+zheng0225@users.noreply.github.com>
2023-11-06 15:56:07 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{   int f;
+    float a;
+    scanf("%d",&f);
+    if(f<800)
+    {
+
+     a=f*0.9;}
+
+    if(f>=800&&f<1500)
+    {
+
+
+       a=f*0.81;}
+
+    if(f>=1500){
+
+        a=f*0.79;
+    }
+
+        printf("%0.1f",a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
