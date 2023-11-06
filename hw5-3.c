e4192b34601fb8177832af70c9d2fa99605e0399
zheng0225 <146065408+zheng0225@users.noreply.github.com>
2023-11-06 21:53:46 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,1 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{   int f;
+    float a,b;//f=工時 a=時薪
+    scanf("%d",&f);
+    scanf("%f",&a);
+       if (f<=60)
+       {
+
+          b =a*f;
+
+
+       }
+       if(f>60&&f<=120)
+       {
+
+          b=60*a+(f-60)*a*1.33;
+
+
+       }
+     if(f>120)
+       {
+
+          b=60*a+60*1.33*a+(f-120)*a*1.66;
+
+
+       }
+
+
+
+
+
+
+
+
+
+
+
+        printf("%0.1f",b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
